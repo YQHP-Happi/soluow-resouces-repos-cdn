9e00c4166dffc0b050c65aa20992b02e2c113e8f
YQHP-Happi <42676023+YQHP-Happi@users.noreply.github.com>
2020-06-03 10:02:41 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -127,4 +127,10 @@ $(function () {
         location.href = location.protocol + '//' + location.host + location.pathname + '?pw=' + $itemPwd.val();
     }
 
+    siteInfoLog("温馨提示", "感谢您访问搜罗资源网！");
+
+    function siteInfoLog(prefix, suffix) {
+        console.log("%c%c" + prefix + "%c" + suffix, "line-height:26px;", "line-height:16px;padding:2px 6px;border-radius: 5px 0px 0px 5px;background:#35495e;color:#fff;font-size:12px;", "padding:2px 8px;background:#0093ff;color:#fff;line-height:16px;font-size:12px;border-radius: 0px 5px 5px 0px;");
+    }
+
 });
